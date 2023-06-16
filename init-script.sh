5bee3e480caf8e7b95ee2facdad8bd79ada7b241
lingcode72 <ling.curty.simplon@gmail.com>
2023-06-16 09:44:46 +0200

    init-script modified

--- init-script.sh
+++ init-script.sh
@@ -5,10 +5,10 @@ sudo sh install-docker.sh
 sudo docker run -d -p 1234:80 --name py_slim ling69/python:3.9-slim
 
 #tage docker
-sudo docker tag py-slim ling69/python:3.9-slim
+#sudo docker tag py-slim ling69/python:3.9-slim
 
 #connection & permision with dockhub
-sudo docker login -u ling69 -p dckr_pat_EQPoqTNrIv7Hb3nFr4Pg5_yVxOs
+#sudo docker login -u ling69 -p dckr_pat_EQPoqTNrIv7Hb3nFr4Pg5_yVxOs
 
 #push image to dockhub
-sudo docker push ling69/py-slim:last
+#sudo docker push ling69/py-slim:last
