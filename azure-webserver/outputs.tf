44c69bd386d2417b4d1172aef1f60edcd0995aa3
lingcode72 <ling.curty.simplon@gmail.com>
2023-06-16 09:32:58 +0200

    add azure-webbserver

---
+++ azure-webserver/outputs.tf
@@ -0,0 +1,24 @@
+output "The_subnet_ID" {
+ value = azurerm_subnet.webserver-subnet.id
+}
+
+output "The_vnet_ID" {
+ value = azurerm_virtual_network.webserver-net.id
+}
+
+output "The_websrever_Private_ip" {
+   value = azurerm_linux_virtual_machine.nginx.private_ip_address
+}
+
+output "The_webserver_Public_ip" {
+   value = azurerm_linux_virtual_machine.nginx.public_ip_address
+}
+
+output "Resource_Group_name" {
+   value = azurerm_resource_group.webserver
+}
+
+output "environment" {
+   value = var.environment
+
+}
