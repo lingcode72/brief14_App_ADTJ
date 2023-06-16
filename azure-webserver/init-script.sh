3cc6cd3d44e0ddbdeaa62a6188799bd274528cb7
lingcode72 <ling.curty.simplon@gmail.com>
2023-06-16 15:28:02 +0200

    modification init-script

--- azure-webserver/init-script.sh
+++ azure-webserver/init-script.sh
@@ -1,5 +1,6 @@
+#Installing Docker
+curl -fsSL https://get.docker.com -o install-docker.sh
 sudo sh install-docker.sh
-
 
 #Creating container
 sudo docker run -d -p 80:1234 --name py_slim ling69/python:3.9-slim
