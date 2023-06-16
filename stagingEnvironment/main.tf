a0b1cd7a71c7e1c36dbe69094d3db90d3f1cad6f
lingcode72 <ling.curty.simplon@gmail.com>
2023-06-16 14:17:23 +0200

    staging main modif

--- stagingEnvironment/main.tf
+++ stagingEnvironment/main.tf
@@ -6,11 +6,11 @@ terraform {
 }
 
 provider "azurerm" {
-   subscription_id = var.subscription_id
+   #subscription_id = var.subscription_id
    #client_id = var.client_id
    #client_secret = var.client_secret
-   tenant_id = var.tenant_id
-   skip_provider_registration = true
+   #tenant_id = var.tenant_id
+   #skip_provider_registration = true
    features {}
 }
 
