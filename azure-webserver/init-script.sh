6d5ed8161eb578b116b8392018d341ea1e112c56
lingcode72 <ling.curty.simplon@gmail.com>
2023-06-18 11:43:51 +0200

    remove command from init-script

--- azure-webserver/init-script.sh
+++ azure-webserver/init-script.sh
@@ -1,15 +1,7 @@
+
 #Installing Docker
 curl -fsSL https://get.docker.com -o install-docker.sh
 sudo sh install-docker.sh
 
 #Creating container
 sudo docker run -d -p 80:1234 --name py_slim ling69/py_slim
-
-#tage docker
-#sudo docker tag py-slim ling69/python:3.9-slim
-
-#connection & permision with dockhub
-#sudo docker login -u ling69 -p dckr_pat_EQPoqTNrIv7Hb3nFr4Pg5_yVxOs
-
-#push image to dockhub
-#sudo docker push ling69/py-slim:last
